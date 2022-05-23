fb815dfe20a53797613c83e24bf739bc97007cc4
Deren <chengderen953@gmail.com>
2022-05-23 21:17:50 +0800

    Add data type validation folder

---
+++ data type validation/data type validation.py
@@ -0,0 +1,16 @@
+def isFloat(num):
+  try:
+    float(num)
+    return True
+  except ValueError:
+    return False
+
+def isInt(num):
+  try:
+    int(num)
+    return True
+  except ValueError:
+    return False
+
+def isNumber(num):
+  return isInt(num) or isFloat(num)
